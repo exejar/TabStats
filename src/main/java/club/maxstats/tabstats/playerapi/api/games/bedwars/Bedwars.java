d12bbfdf65cc36a374c86e3b3e9aa57cda85de7c
exejar <themmludwig@gmail.com>
2021-11-03 04:55:15 -0400

    Added star unicode to star stat

--- src/main/java/club/maxstats/tabstats/playerapi/api/games/bedwars/Bedwars.java
+++ src/main/java/club/maxstats/tabstats/playerapi/api/games/bedwars/Bedwars.java
@@ -102,7 +102,7 @@ public class Bedwars extends BedwarsUtil {
     @Override
     public void setFormattedStatList() {
         StatString star = new StatString("Star");
-        star.setValue(this.getStarColor(((StatInt)this.star).getValue()).toString() + ((StatInt)this.star).getValue());
+        star.setValue(this.getStarColor(((StatInt)this.star).getValue()).toString() + ((StatInt)this.star).getValue() + "\u272B");
         this.formattedStatList.add(star);
 
         StatString ws = new StatString("WS");
