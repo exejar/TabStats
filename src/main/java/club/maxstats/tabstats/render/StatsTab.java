203990cc67759ae29dc3b57e85093bf3609f0e7d
Directives <miles.bhattacharya1@gmail.com>
2021-11-14 16:09:41 -0800

    Matched up mcmod.info desc with README, move tab back to original location

--- src/main/java/club/maxstats/tabstats/render/StatsTab.java
+++ src/main/java/club/maxstats/tabstats/render/StatsTab.java
@@ -28,10 +28,13 @@ import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.IChatComponent;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.WorldSettings;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.InputEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.text.WordUtils;
+import org.lwjgl.input.Keyboard;
 
 import java.util.Comparator;
 import java.util.List;
@@ -47,8 +50,6 @@ public class StatsTab extends GuiPlayerTabOverlay {
     private long lastTimeOpened;
     /** Whether or not the playerlist is currently being rendered */
     public boolean tabBeingRendered;
-    /**Whether or not to move the tablist down (we'll add an option in /tabstats gui)**/
-    public boolean moveTabDown;
     /* whether or not rank should come before color prefix */
     private boolean rankBeforePrefix = false;
     private final int entryHeight = 12;
@@ -72,12 +73,7 @@ public class StatsTab extends GuiPlayerTabOverlay {
         ScaledResolution scaledRes = new ScaledResolution(this.mc);
         /* where the render should start on x plane */
         int startingX = scaledRes.getScaledWidth() / 2 - width / 2;
-        /* where the render should start on y plane (either below bossbar or at the top with a hidden bossbar)*/
-        // if (moveTabDown) {
-        int startingY = 36;
-        // } else {
-        // int startingY = 12;
-        // }
+        int startingY = 35;
 
         /* this is kind of useless...as nameWidth and objectiveWidth aren't used */
         for (NetworkPlayerInfo playerInfo : playerList) {
@@ -313,7 +309,7 @@ public class StatsTab extends GuiPlayerTabOverlay {
         String playerRank = hPlayer.getPlayerRank();
 
         if (team != null) {
-            /** remove [NON] as it's not shown in regular tab **/
+            /** remove [NON] as it's not shown in regular tab */
             String colorPrefix = team.getColorPrefix();
             if (ChatColor.stripColor(colorPrefix).contains(ChatColor.stripColor(playerRank))) {
                 playerRank = "";
